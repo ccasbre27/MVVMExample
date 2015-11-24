b1ffce8e5bf2ecaabe2c9834570d3fb5e54404ff
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-24 11:35:31 -0600

    Init

---
+++ MVVMExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace MVVMExample.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private string _helloWorld;
+
+        public string HelloWorld
+        {
+            get
+            {
+                return _helloWorld;
+            }
+            set
+            {
+                Set(() => HelloWorld, ref _helloWorld, value);
+            }
+        }
+
+        public MainViewModel()
+        {
+            HelloWorld = IsInDesignMode
+                ? "Runs in design mode"
+                : "Runs in runtime mode";
+        }
+    }
+
+}
